1774811db94682eb0b77db2509b11ecedea27494
pionehoangkhai <pionehoangkhai@gmail.com>
2025-06-16 18:01:36 +0700

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "EG-GROUP",
-  description: "EG-GROUP is a EG in IoT, AI and Blockchain applications, developing multi-platform technology solutions, bringing transparency, security and seamless experience to users.",
+  title: "EG GROUP",
+  description: "EG GROUP là một tập thể tiên phong trong lĩnh vực phát triển phần mềm, quy tụ những con người đam mê sáng tạo, đổi mới và không ngừng học hỏi. Với tầm nhìn vượt ra ngoài biên giới Việt Nam, chúng tôi mang trong mình khát vọng xây dựng những giải pháp phần mềm tiên tiến, đáp ứng nhu cầu của các doanh nghiệp trong và ngoài nước. Bằng tinh thần đoàn kết và quyết tâm mạnh mẽ, EG GROUP không ngừng nỗ lực để đưa sản phẩm phần mềm vươn tầm quốc tế.",
 };
 
 export default function RootLayout({
